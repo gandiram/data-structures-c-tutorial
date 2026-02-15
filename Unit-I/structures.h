36d01888c746a0ad27b90ec68476eabb8b3a6551
gandiram <136839844+gandiram@users.noreply.github.com>
2026-02-15 17:23:42 +0530

    Create structures.h header file with structure definitions, union definition, enumeration, and function prototypes

--- Unit-I/structures.h
+++ Unit-I/structures.h
@@ -1,1 +1,41 @@
-#ifndef STRUCTURES_H\n#define STRUCTURES_H\n\n// Structure definition for a node in a singly linked list\ntypedef struct Node {\n    int data;\n    struct Node* next;\n} Node;\n\n// Union definition for holding different data types\ntypedef union Data {\n    int intValue;\n    float floatValue;\n    char charValue;\n} Data;\n\n#endif // STRUCTURES_H
+// structures.h
+
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure definition for Person
+struct Person {
+    char name[50];
+    int age;
+    char gender;
+};
+
+// Structure definition for Student
+struct Student {
+    struct Person personInfo;
+    int studentID;
+    float gpa;
+};
+
+// Union definition for Data
+union Data {
+    int intValue;
+    float floatValue;
+    char strValue[50];
+};
+
+// Enumeration for Color
+enum Color {
+    RED,
+    GREEN,
+    BLUE
+};
+
+// Function prototypes
+void printPerson(struct Person p);
+void printStudent(struct Student s);
+
+#endif // STRUCTURES_H
